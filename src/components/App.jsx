fcab446177d51ec21553bf7641d33233ab72a4cb
Tetiana2023 <drofaj18@gmail.com>
2023-03-07 17:50:26 +0200

    second part

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 // import css from './Statistic.module.css';
 
-export class  App extends React.Component {
+export class App extends React.Component {
   state = {
     good: 0,
     neutral: 0,
@@ -10,32 +10,35 @@ export class  App extends React.Component {
   };
 
   handleIncrementGood = () => {
-    this.setState(prevState => {
-      return {
-        good: prevState.good + 1,
-      };
-    });
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
   };
   handleIncrementNeutral = () => {
-    this.setState(prevState => {
-      return {
-        neutral: prevState.neutral + 1,
-      };
-    });
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
   };
 
   handleIncrementBad = () => {
-    this.setState(prevState => {
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
   };
-render(){
-  return (
-    <div className='Statistic'>
-     
-     <h1 className="title">Please leave feedback</h1>
+
+  countTotalFeedback = () => {
+    const { good, neutrale, bad } = this.state;
+    return +(good + neutrale + bad);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+       return (
+      <div className="Statistic">
+        <h1 className="title">Please leave feedback</h1>
         <div>
           <button type="button" onClick={this.handleIncrementGood}>
             Good
@@ -46,15 +49,18 @@ render(){
           <button type="button" onClick={this.handleIncrementBad}>
             Bad
           </button>
-    </div> 
-  
-    <div>
-    <h2 className="title">Statistics</h2>
-    <p className="text"> Good:{this.state.good}</p>
-    <p className="text"> Neutrale:{this.state.neutral}</p>
-    <p className="text"> Bad:{this.state.bad}</p>
-    </div>
-    </div>)
-    
-  };
+        </div>
+
+        <div>
+          <h2 className="title">Statistics</h2>
+          <p className="text"> Good:{this.state.good}</p>
+          <p className="text"> Neutrale:{this.state.neutral}</p>
+          <p className="text"> Bad:{this.state.bad}</p>
+          <p className="text"> Total:{this.countTotalFeedback()}</p>
+          <p className="text"> Positive feedback:{this.countPositiveFeedbackPercentage()}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
